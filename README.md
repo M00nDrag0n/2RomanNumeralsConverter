fe4fdc857391e72ae1ae80a45b831b772a922416
Student <example@example.com>
2019-10-31 10:22:40 -0700

    add everything at once, up to the current stopping point. Many arrays and variables with the attempted start of a function.

---
+++ README.md
@@ -0,0 +1,18 @@
+
+List of Specs
+
+The program converts numbers to single letter roman numerals.
+Input: 1, 5, 10, 50, 100, 500, 1,00
+Output: I, V, X, L, C, D, M
+
+The program converts single-digit numbers to multi-letter roman numerals.
+Input: 1, 2, 3, 4, 6, 7, 8, 9
+Output: II, III, IV, VI, VII, VIII, IX
+
+The program converts double-digit numbers to multi-letter roman numerals.
+Input: 11, 12, 13, 14, 15, 16, 17, 18, 19
+Output: XI, III, IV, VI,XV VII, VIII, IXX
+
+The program converts triple-digit numbers to multi-letter roman numerals
+Input: 108
+Output: CVIII
---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+  $("#inputs").submit(function(event) {
+    event.preventDefault();
+
+    var conversions = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var is = ["1", 1];
+    var vs = ["5", 5];
+    var xs = ["10", 10];
+    var ls = ["50", 50];
+    var cs = ["100", 100];
+    var ds = ["500", 500];
+    var ms = ["1000", 1000];
+    var one = ["I"];
+    var five = ["V"];
+    var ten = ["X"];
+    var fifty = ["L"];
+    var hundred = ["C"];
+    var fiveHundred = ["D"];
+    var thousand = ["M"];
+
+    "V", "X", "L", "C", "D", "M"];
+
+
+
+    var input1 = $("#input1").val();
+
+    var output = function(input1) {
+      return
+    }
+
+    //"1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"//
+    // PUT THE COMMA BACK ON THE 9 WHEN UNHIDING THE REST OF THE ARRAY, ANISE
+
+
+
+
+    //
+    // if () {
+      //
+      // }
+
+      $("#output").text(output)
+
+    });
+  })
